5d5920e911e46b027e386ce40a6e253325b4b4da
Ariel Marcus <ajmarcus@users.noreply.github.com>
2020-09-26 03:13:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = os.environ.get('NAME', 'World')
-    return 'Michael {}!'.format(name)
+    return 'Ariel {}!'.format(name)
 
 
 if __name__ == "__main__":
